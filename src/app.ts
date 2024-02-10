425fe696855d1a1c70d2d93ede3460f126d7d39b
yasser110306 <yasserrabie110306@gmail.com>
2024-02-10 15:08:41 +0200

    updated code

--- src/app.ts
+++ src/app.ts
@@ -18,12 +18,15 @@ const list = new ListTemplate(ul);
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
 
+    let values: [string, string, number];
+    values = [toFrom.value, details.value, amount.valueAsNumber];
+
     let doc: HasFormatter;
 
     if (type.value === "invoice") {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     } else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
 
     list.render(doc, type.value, "start");
